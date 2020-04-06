1b2227d5bbeb68d9f074a7fe3a8dadf00e4af469
leonel040792 <tang.leonel@gmail.com>
2020-04-06 00:52:25 -0400

    modificationMainPage
    Uploaded the Logo for the top bar, also cleaned up a little (not entirely) the index.html code and added some transitions

---
+++ firstProject/script/functions.js
@@ -0,0 +1,20 @@
+//DOM
+//querySelector - querySelector All (multiple variables)
+
+let container = document.querySelectorAll("aquiAntesEstabaA");
+
+/*container.forEach(function(second){
+
+ console.log(second);
+
+ });*/
+
+ container.forEach(function(aquiAntesEstabaA){
+
+ 	
+ 	a.addEventListener('click', function(ev){
+
+ 		ev.preventDefault();
+ 		console.log(this);
+ 	})
+ });
